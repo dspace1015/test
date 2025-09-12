f27a1486a7fdc52583c0215aca03bbec50c52a72
Snipershot8913 <153301576+Snipershot8913@users.noreply.github.com>
2025-09-12 14:25:38 -0500

    Update code.js

--- code.js
+++ code.js
@@ -8,5 +8,5 @@ function drawLine(x1,y1,x2,y2,color,width){
   draw.lineTo(x2,y2);
   draw.stroke();
 }
-drawLine(0,0,200,200,"Red",3);
-drawLine(200,0,0,200,"Green",3);
+drawLine(0,0,700,500,"Red",3);
+drawLine(500,0,0,700,"Green",3);
