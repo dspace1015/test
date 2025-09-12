0162408ee5a2f2ac1617e78ab19a254caf241956
dspace1015 <drstromlund@gmail.com>
2025-09-12 14:28:27 -0500

    Update code.js

--- code.js
+++ code.js
@@ -3,7 +3,7 @@ var canvas = document.getElementById("Main_Canvas");
 var draw = canvas.getContext("2d");
 function drawLine(x1,y1,x2,y2,color,width){
   draw.strokeStyle = color;
-  draw.strokeWidth = width;
+  draw.lineWidth = width;
   draw.moveTo(x1,y1);
   draw.lineTo(x2,y2);
   draw.stroke();
