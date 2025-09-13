d7328e72b94d8a4a169ece06d4ce6ea5c0db19b3
dspace1015 <drstromlund@gmail.com>
2025-09-12 19:38:20 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,7 @@
 //Setup canvas for drawing
 var canvas = document.getElementById("Main_Canvas");
 var draw = canvas.getContext("2d");
+var gl = canvas.getContext("webgl")
 function drawLine(x1,y1,x2,y2,color,width){
   draw.beginPath();
   draw.strokeStyle = color;
@@ -9,5 +10,10 @@ function drawLine(x1,y1,x2,y2,color,width){
   draw.lineTo(x2,y2);
   draw.stroke();
 }
+if(!gl){
+  alert("Error, your browser does not support webgl. Try downloading the newest version of your browser or if this worked before try again later");
+}
 drawLine(0,0,700,500,"#ff0000",3);
 drawLine(0,500,700,0,"#000000",3);
+gl.clearColor(1,0,0,1);
+
