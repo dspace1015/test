e11bb858d879a3cb7bb95de0c4bdbb8424be253f
dspace1015 <drstromlund@gmail.com>
2025-09-12 19:07:13 -0500

    Fixed draw colors

--- code.js
+++ code.js
@@ -2,12 +2,12 @@
 var canvas = document.getElementById("Main_Canvas");
 var draw = canvas.getContext("2d");
 function drawLine(x1,y1,x2,y2,color,width){
+  draw.beginPath();
   draw.strokeStyle = color;
   draw.lineWidth = width;
   draw.moveTo(x1,y1);
   draw.lineTo(x2,y2);
   draw.stroke();
 }
-drawLine(0,0,700,500,"red",3);
-drawLine(0,500,700,0,"green",3);
-
+drawLine(0,0,700,500,"#ff0000",3);
+drawLine(0,500,700,0,"#000000",3);
