78f9c882e688c3c9bc0e88e2fedd8977ff786f93
JiffyRob <101531330+JiffyRob@users.noreply.github.com>
2025-09-18 17:53:59 -0500

    moved shader code into separate files for ease of linting and editing

--- code.js
+++ code.js
@@ -1,119 +1,114 @@
-//Setup canvas for drawing
-var canvas = document.getElementById("Main_Canvas");
-//var draw = canvas.getContext("2d");
-var gl = canvas.getContext("webgl");
-if(!gl){
-  alert("Error, your browser does not support webgl. Try downloading the newest version of your browser or if this worked before try again later");
-}
-var vertices = [
-                0.0,0.5,0.0,
-                -0.5,-0.5,0.0,
-                0.5,-0.5,0.0, 
-                             ];
-var indices = [0,1,2];
+async function test() {
+    //Setup canvas for drawing
+    var canvas = document.getElementById("Main_Canvas");
+    //var draw = canvas.getContext("2d");
+    var gl = canvas.getContext("webgl");
+    if(!gl){
+      alert("Error, your browser does not support webgl. Try downloading the newest version of your browser or if this worked before try again later");
+    }
+    var vertices = [
+                    0.0,0.5,0.0,
+                    -0.5,-0.5,0.0,
+                    0.5,-0.5,0.0, 
+                                ];
+    var indices = [0,1,2];
 
-// Create an empty buffer object to store vertex buffer
-var vertex_buffer = gl.createBuffer();
+    // Create an empty buffer object to store vertex buffer
+    var vertex_buffer = gl.createBuffer();
 
-// Bind appropriate array buffer to it
-gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+    // Bind appropriate array buffer to it
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
 
-// Pass the vertex data to the buffer
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    // Pass the vertex data to the buffer
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
-// Unbind the buffer
-gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    // Unbind the buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
-// Create an empty buffer object to store Index buffer
-var Index_Buffer = gl.createBuffer();
+    // Create an empty buffer object to store Index buffer
+    var Index_Buffer = gl.createBuffer();
 
-// Bind appropriate array buffer to it
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
+    // Bind appropriate array buffer to it
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
 
-// Pass the vertex data to the buffer
-gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+    // Pass the vertex data to the buffer
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
 
- // Unbind the buffer
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+    // Unbind the buffer
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
 
-/*================ Shaders ====================*/
-// Vertex shader source code
-var vertCode =
-'attribute vec3 coordinates;' +
-				
-'void main(void) {' +
-' gl_Position = vec4(coordinates, 1.0);' +
-'}';
-   
-  // Create a vertex shader object
-var vertShader = gl.createShader(gl.VERTEX_SHADER);
+    /*================ Shaders ====================*/
 
-// Attach vertex shader source code
-gl.shaderSource(vertShader, vertCode);
+    // TODO: Batch these fetches into one async call?
+    // Load vertex shader code
+    const vertCode = await fetch("vertex_shader.vert").then(response => response.text());
+    // Load fragment shader code
+    const fragCode = await fetch("fragment_shader.frag").then(response => response.text());
 
-// Compile the vertex shader
-gl.compileShader(vertShader);
+    // Create a vertex shader object
+    vertShader = gl.createShader(gl.VERTEX_SHADER);
+    // Attach vertex shader source code
+    gl.shaderSource(vertShader, vertCode);
+    // Compile the vertex shader
+    gl.compileShader(vertShader);
 
-//fragment shader source code
-var fragCode =
-   'void main(void) {' +
-     ' gl_FragColor = vec4(0.0, 0.0, 0.0, 0.1);' +
-  '}';
+    // Create fragment shader object
+    var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+    // Attach fragment shader source code
+    gl.shaderSource(fragShader, fragCode); 
             
-// Create fragment shader object
-var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+    // Compile the fragmentt shader
+    gl.compileShader(fragShader);
 
-// Attach fragment shader source code
-gl.shaderSource(fragShader, fragCode); 
-         
-// Compile the fragmentt shader
-gl.compileShader(fragShader);
+    // Create a shader program object to store
+    // the combined shader program
+    var shaderProgram = gl.createProgram();
 
-// Create a shader program object to store
-// the combined shader program
-var shaderProgram = gl.createProgram();
+    // Attach a vertex shader
+    gl.attachShader(shaderProgram, vertShader);
 
-// Attach a vertex shader
-gl.attachShader(shaderProgram, vertShader);
+    // Attach a fragment shader
+    gl.attachShader(shaderProgram, fragShader);
 
-// Attach a fragment shader
-gl.attachShader(shaderProgram, fragShader);
+    // Link both the programs
+    gl.linkProgram(shaderProgram);
 
-// Link both the programs
-gl.linkProgram(shaderProgram);
+    // Use the combined shader program object
+    gl.useProgram(shaderProgram);
 
-// Use the combined shader program object
-gl.useProgram(shaderProgram);
+    /*======= Associating shaders to buffer objects =======*/
 
-/*======= Associating shaders to buffer objects =======*/
+    // Bind vertex buffer object
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
 
-// Bind vertex buffer object
-gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+    // Bind index buffer object
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
+            
+    // Get the attribute location
+    var coord = gl.getAttribLocation(shaderProgram, "coordinates");
 
-// Bind index buffer object
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
-         
-// Get the attribute location
-var coord = gl.getAttribLocation(shaderProgram, "coordinates");
+    // Point an attribute to the currently bound VBO
+    gl.vertexAttribPointer(coord, 3, gl.FLOAT, false, 0, 0); 
+            
+    // Enable the attribute
+    gl.enableVertexAttribArray(coord);
 
- // Point an attribute to the currently bound VBO
-gl.vertexAttribPointer(coord, 3, gl.FLOAT, false, 0, 0); 
-         
-// Enable the attribute
-gl.enableVertexAttribArray(coord);
+    /*=========Drawing the triangle===========*/
+    // Clear the canvas
+    gl.clearColor(0.5, 0.5, 0.5, 0.9);
 
-/*=========Drawing the triangle===========*/
- // Clear the canvas
-gl.clearColor(0.5, 0.5, 0.5, 0.9);
+    // Enable the depth test
+    gl.enable(gl.DEPTH_TEST);
 
-// Enable the depth test
-gl.enable(gl.DEPTH_TEST);
+    // Clear the color buffer bit
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
-// Clear the color buffer bit
-gl.clear(gl.COLOR_BUFFER_BIT);
+    // Set the view port
+    gl.viewport(0,0,canvas.width,canvas.height);
 
-// Set the view port
-gl.viewport(0,0,canvas.width,canvas.height);
+    // Draw the triangle
+    gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT,0);
+}
 
-// Draw the triangle
-gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT,0);
+test();
---
+++ vertex_shader.vert
@@ -0,0 +1,7 @@
+#version 100
+
+attribute vec3 coordinates;
+
+void main(void) {
+    gl_Position = vec4(coordinates, 1.0);
+}
