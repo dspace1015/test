1918573edee1189cca7ecb4acb7a2d18f482d91f
dspace1015 <drstromlund@gmail.com>
2025-10-10 13:25:54 -0500

    Update code.js

--- code.js
+++ code.js
@@ -10,13 +10,12 @@ const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-function Init(){
-  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  const cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
-  camera.position.z = 3;
-}
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+camera.position.z = 3;
+
 //Main loop for project
 function main() {
   Update();
@@ -26,5 +25,4 @@ function Update(){
   cube.rotation.x += 0.6/60;
   cube.rotation.y += 0.6/60;
 }
-Init();
 renderer.setAnimationLoop( main );
