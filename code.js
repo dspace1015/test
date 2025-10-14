f0fa81a23c6d1e9eda4b24839814a1c3bcca7414
dspace1015 <drstromlund@gmail.com>
2025-10-14 14:09:51 -0500

    Update code.js
    Attempted adding parametric geometry

--- code.js
+++ code.js
@@ -10,7 +10,17 @@ const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+//Test making new geometry
+function terrain(u,v,target){
+  u*= math.PI;
+  v*= 2*math.PI;
+  let h = 0.1*math.sin(10*u);
+  let x = (h+1)*math.cos(v)*math.cos(u);
+  let y = (h+1)*math.sin(v)*math.cos(u);
+  let z = (h+1)*math.sin(u);
+  target.set( x, y, z );
+}
+const geometry = new THREE.ParametricGeometry(terrain, 50, 50);
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
