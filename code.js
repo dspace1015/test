455e18743642f743d042eea74719621f077f5f2e
dspace1015 <drstromlund@gmail.com>
2025-10-15 13:12:26 -0500

    Added Comments
    Added Comments and changed a few variables

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
 import * as THREE from 'three';
 //Import addon for parametric surfaces
 import { ParametricGeometry } from 'three/addons/geometries/ParametricGeometry.js';
-
 //Import Three js object loader 
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
 //create the scene
 const scene = new THREE.Scene();
 //Define the camera
@@ -14,26 +14,34 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 //defining texuter loader
 const loader = new THREE.TextureLoader();
+
 //Loading textures:
 const Earth_tex = loader.load("models/2k_earth_daymap.jpg");
-//Test making new geometry, defining surface function
+
+//Defining the terrain function
 function terrain(u,v,target){
   u*= 2*Math.PI;
   v = 1-v;
   v*= Math.PI;
+  //Calculates position of vertex off of u,v
   let x = Math.cos(u)*Math.sin(v);
-  let y = Math.sin(u)*Math.sin(v);
-  let z = Math.cos(v);
-  let h = 0.05*Math.sin(Math.PI*x*3)+0.05*Math.cos(Math.PI*y*10)+0.05*Math.cos(Math.PI*z*15+2);
+  let z = -Math.sin(u)*Math.sin(v);
+  let y = Math.cos(v);
+  //Height function defined in terms of x,y,z
+  let h = 0.05*Math.sin(Math.PI*x*3)+0.05*Math.cos(Math.PI*z*10)+0.05*Math.cos(Math.PI*y*15+2);
+  //Scaling by height
   x = (1+h)*x;
   y = (1+h)*y;
   z = (1+h)*z;
   target.set( x, y, z );
 }
+//Define geometry and Create Earth object
 const geometry = new ParametricGeometry(terrain, 500, 500);
 const material = new THREE.MeshBasicMaterial({map: Earth_tex},{side:THREE.BackSide});
 const Earth = new THREE.Mesh( geometry, material );
 scene.add( Earth );
+
+//Set camera position
 camera.position.z = 3;
 
 //Main loop for project
@@ -42,7 +50,7 @@ function main() {
   renderer.render( scene, camera );
 }
 function Update(){
-  Earth.rotation.x += 0.6/60;
+  //Earth.rotation.x += 0.6/60;
   Earth.rotation.y += 0.6/60;
 }
 renderer.setAnimationLoop( main );
