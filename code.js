ce3eb70a71b774576dd635ed70fd3f06972994c0
dspace1015 <drstromlund@gmail.com>
2025-10-17 19:52:46 -0500

    Update code.js
    Made some new changes and added mouse control

--- code.js
+++ code.js
@@ -7,7 +7,11 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 //create the scene
 const scene = new THREE.Scene();
 //Define the camera
-const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 );
+let fov = 60;
+let near = 0.1;
+let far = 1000;
+let aspect = window.innerWidth / window.innerHeight;
+const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
 //Define renderer and properties
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -17,7 +21,15 @@ const loader = new THREE.TextureLoader();
 
 //Loading textures:
 const Earth_tex = loader.load("models/2k_earth_daymap.jpg");
-
+//Defining critical functions:
+function RotateM(M,axis,ang){
+  if(axis=="x"){
+    return [M[0],M[1]*Math.cos(ang)-M[2]*Math.sin(ang),M[2]*Math.cos(ang)+M[1]*Math.sin(ang),M[3],
+            M[4],M[5]*Math.cos(ang)-M[6]*Math.sin(ang),M[6]*Math.cos(ang)+M[5]*Math.sin(ang),M[7],
+            M[8],M[9]*Math.cos(ang)-M[10]*Math.sin(ang),M[10]*Math.cos(ang)+M[9]*Math.sin(ang),M[11],
+            M[12],M[13],M[14],M[15]];
+  }
+}
 //Defining the terrain function
 function terrain(u,v,target){
   u*= 2*Math.PI;
@@ -43,10 +55,10 @@ scene.add( Earth );
 
 //Set camera position
 camera.position.z = 3;
-//set up key press listeners
-var pressedKeys = {};
-var mouse = {x:0,y:0,d:false};
-var mouseOld = mouse;
+//set up key press listeners and mouse event listeners
+let pressedKeys = {};
+let mouse = {x:0,y:0,d:false};
+let mouseOld = {x:0,y:0,d:false};
 window.onkeyup = function(e) { pressedKeys[e.keyCode] = false; }
 window.onkeydown = function(e) { pressedKeys[e.keyCode] = true; }
 window.onmousemove = function(e) { mouse.x = e.clientX; mouse.y = e.clientY; }
@@ -63,15 +75,19 @@ function main() {
   dt = Time - OldTime;
   OldTime = Time;
   Update();
-  mouseOld = mouse;
   renderer.render( scene, camera );
 }
 function Update(){
-  if(true){
-    Earth.rotation.x += (mouse.y - mouseOld.y)*0.06;
-    Earth.rotation.y += (mouse.x - mouseOld.x)*0.06;
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  if(mouse.d){
+    camera.rotation.x += Math.PI*(mouse.y - mouseOld.y)/window.innerHeight;
+    camera.rotation.y += Math.PI*(mouse.x - mouseOld.x)/window.innerHeight;
   }
-  //Earth.rotation.x += 0.06*dt;
-  //Earth.rotation.y += 0.6*dt;
+  console.log(camera.matrixWorld);
+  mouseOld.x = mouse.x;
+  mouseOld.y = mouse.y;
+  mouseOld.d = mouse.d;
 }
 renderer.setAnimationLoop( main );
